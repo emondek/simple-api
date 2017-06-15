bc8cbe33ea907f85aa6c85d2c56191994c70deba
Ed Mondek <emondek@hotmail.com>
2017-06-15 10:57:07 -0500

    Dockerized the app

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var app = express()
+var ip = require('ip')
+var os = require('os')
+
+app.get('/', function (req, res) {
+    res.send('Simple API says "Hello!"')
+})
+
+app.get('/server/url', function (req, res) {
+    res.send(req.url)
+})
+
+app.get('/server/ipaddress', function (req, res) {
+    res.send(ip.address())
+})
+
+app.get('/server/hostname', function (req, res) {
+    res.send(os.hostname())
+})
+
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+})
